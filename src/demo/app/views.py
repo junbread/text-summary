af2826fb7d440a2894e66f1d0bfbf63bc8ab54ae
anah2run <anah2run@gmail.com>
2020-02-06 11:14:08 +0900

    데모 페이지 사용자 평가 기능 추가

--- src/demo/app/views.py
+++ src/demo/app/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
-from src.summarize import summarize
+from django.shortcuts import redirect
+import time
+from .models import Eval
+# from src.summarize import summarize
 
 def demo(request):
     if(request.method == 'GET'):
         summary = ""
         document = ""
-        answer = ""
     else:
         document = request.POST['document']
-        answer = request.POST['answer']
-        summary = summarize.summarize(document)['pgn']
+        summary = "summary"
+        # summary = summarize.summarize(document)['pgn']
 
-    return render(request, 'demo.html', {'summary': summary, 'document': document, 'answer': answer})
+    return render(request, 'demo.html', {'summary': summary, 'document': document})
+def eval(request):
+    
+    eval_label = request.POST['eval']
+    if eval_label == '최악':
+        score = 0
+    elif eval_label == '미흡':
+        score = 1
+    elif eval_label == '보통' :
+        score = 2
+    elif eval_label == '좋음' :
+        score = 3
+    elif eval_label == '최고' :
+        score = 4
+    post = Eval()
+    post.score = score
+    post.name = request.POST['name']
+    post.sum = request.POST['sum']
+    post.doc = request.POST['doc']
+    post.save()
+    return redirect('demo')
--- src/demo/urls.py
+++ src/demo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from app import views
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.demo, name='app')
+    path('', views.demo, name='demo'),
+    path('eval', views.eval, name='eval'),
+    path('admin/', admin.site.urls)
 ]
