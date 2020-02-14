532b904e38ada758c855fb8197ab29fcfe6a2f71
JunBread <junbbang5@gmail.com>
2020-02-14 16:18:04 +0900

    summarize option 추가

--- src/summarize/summarize.py
+++ src/summarize/summarize.py
@@ -1,24 +1,33 @@
-from .preprocess import *
+from . import preprocess
+
 from .pgn.util import Decoder as PGNDecoder
 from .textrank.util import Decoder as TextrankDecoder
-
 #from .baseline import BaselineDecoder as BaselineDecoder
 
 
 class Summarizer(object):
     def __init__(self):
-        init_libraries()
+        preprocess.init_libraries()
+
         self.pgn_decoder = PGNDecoder()
         self.textrank_decoder = TextrankDecoder()
+        # self.baseline_decoder = BaselineDecoder()
 
-    def summarize(self, text):
+    def summarize(self, text, options=['pgn', 'textrank']):
         """return summary of given text"""
 
-        preprocessed_text = process(text)
+        preprocessed_text = preprocess.process(text)
 
-        result = {
-            'pgn': self.pgn_decoder.decode(preprocessed_text),
-            'textrank': self.textrank_decoder.decode(preprocessed_text)
+        pgn_result = None
+        textrank_result = None
+
+        if 'pgn' in options:
+            pgn_result = self.pgn_decoder.decode(preprocessed_text)
+
+        if 'textrank' in options:
+            textrank_result = self.textrank_decoder.decode(preprocessed_text)
+        
+        return {
+            'pgn': pgn_result,
+            'textrank': textrank_result
         }
-
-        return result
