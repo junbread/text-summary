d550cfb3c4b7587d12c767641f7801ad5f826f11
JunBread <junbbang5@gmail.com>
2020-02-14 10:25:59 +0900

    master 충돌사항 병합

--- src/demo/app/views.py
+++ src/demo/app/views.py
@@ -4,48 +4,35 @@ from .models import Eval
 from src.summarize.summarize import Summarizer
 
 import time
+
 summarizer = Summarizer()
 
 def demo(request):
-    opt_transformer_checked = None
-    opt_textrank_checked = None
-    summary = None
-    document = None
-    opt_trn = None
-    opt_textrank_checked = None
-    if(request.method == 'GET'):
-        summary = "##요약이 여기에 표시됩니다!"
-        document = ""
-        opt_transformer_checked = "checked"
-        opt_textrank_checked = "checked"
-        trns = 0
-        rnd = 0
-    else:
-        trns = False
-        rnd = False
-        document = request.POST['document']
-        answer = request.POST['answer']
-        summary = summarizer.summarize(document)['pgn']
-        
-    return render(request, 'demo.html', {'summary': summary, 'document': document, 'summary_trns': trns, 'summary_rnd':rnd, 'opt_trns_checked':opt_transformer_checked, 'opt_rnd_checked':opt_textrank_checked})
-  
-def eval(request):
+    return_object = {
+        'article': None,
+        'summary': None,
+        'options': []
+    }
     
-    eval_label = request.POST['eval']
-    if eval_label == '최악':
-        score = 0
-    elif eval_label == '미흡':
-        score = 1
-    elif eval_label == '보통' :
-        score = 2
-    elif eval_label == '좋음' :
-        score = 3
-    elif eval_label == '최고' :
-        score = 4
-    post = Eval()
-    post.score = score
-    post.name = request.POST['name']
-    post.sum = request.POST['sum']
-    post.doc = request.POST['doc']
-    post.save()
+    if(request.method == 'GET'):
+        article = "" # article selection module
+
+    if(request.method == 'POST'):
+        article = request.POST['article']
+        
+        if article is not None:
+            summary = summarizer.summarize(article)
+
+    return render(request, 'demo.html', return_object)
+
+
+def eval(request):
+
+    eval_record = Eval()
+    eval_record.score = int(request.POST['score'])
+    eval_record.name = request.POST['name']
+    eval_record.sum = request.POST['sum']
+    eval_record.doc = request.POST['doc']
+    eval_record.save()
+
     return redirect('demo')
